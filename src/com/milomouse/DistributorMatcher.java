92bbad4860480389f3c0cd80f4d21c7164dcf1c7
LauraBOwensKY <LauraBOwens@gmail.com>
2019-07-23 03:16:57 -0400

    Added .txt files to data folder

--- src/com/milomouse/DistributorMatcher.java
+++ src/com/milomouse/DistributorMatcher.java
@@ -4,8 +4,8 @@ public class DistributorMatcher {
 
     public DistributorMatcher(String distributorFile, String breweryFile) {
         var textFileReader = new TextFileReader();
-        var distributorRecordsHeader = textFileReader.readFile(distributorFile); //First read is the heading line
-        var breweryRecordsHeader = textFileReader.readFile(breweryFile);
+        var distributorRecords = textFileReader.readFile(distributorFile); //First read is the heading line
+        var breweryRecords = textFileReader.readFile(breweryFile);
 
         // Determine which distributors are available to each brewery
         System.out.println(breweryRecordsHeader);
